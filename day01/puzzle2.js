506ed2784684dd5ef27695212a6684a870b143e9
Marc Mignonsin <web@sparring-partner.be>
2023-12-02 21:22:55 +0100

    Day 1: cleanup puzzle 2

--- day01/puzzle2.js
+++ day01/puzzle2.js
@@ -1,40 +1,30 @@
-import { parseInput } from './parseInput.js';
+import { parseInput } from "./parseInput.js";
 // import textInput from './sample2.js';
-import textInput from './input.js';
+import textInput from "./input.js";
 
 const lookup = {
-  one: '1',
-  two: '2',
-  three: '3',
-  four: '4',
-  five: '5',
-  six: '6',
-  seven: '7',
-  eight: '8',
-  nine: '9',
+  one: "1",
+  two: "2",
+  three: "3",
+  four: "4",
+  five: "5",
+  six: "6",
+  seven: "7",
+  eight: "8",
+  nine: "9",
 };
 
-const reverse = (str) => str.split('').reverse().join('');
-
-const reverseLookup = Object.entries(lookup).reduce(
-  (acc, [key, value]) => ({ ...acc, [reverse(key)]: value }),
-  {}
-);
+const firstRegex = new RegExp(`(\[1-9]|${Object.keys(lookup).join("|")})`);
+const lastRegex = new RegExp(`.*(\[1-9]|${Object.keys(lookup).join("|")})`);
 
 function solve(input) {
-  const firstRegex = new RegExp(`(\[1-9]|${Object.keys(lookup).join('|')})`);
-
-  const lastRegex = new RegExp(
-    `(\[1-9]|${Object.keys(reverseLookup).join('|')})`
-  );
-
   let output = 0;
 
   for (const line of input) {
     const [, first] = line.match(firstRegex);
-    const [, last] = reverse(line).match(lastRegex);
+    const [, last] = line.match(lastRegex);
 
-    output += Number((lookup[first] || first) + (reverseLookup[last] || last));
+    output += Number((lookup[first] || first) + (lookup[last] || last));
   }
 
   return output;
@@ -42,4 +32,4 @@ function solve(input) {
 
 const output = solve(parseInput(textInput));
 
-console.log('→', output);
+console.log("→", output);
